6968ab99ad274095b15e81489b141cde7b098143
MADaveee <90868721+MADaveee@users.noreply.github.com>
2021-10-25 00:28:26 +0200

    Update FileInitializer.java

--- src/filesystem/FileInitializer.java
+++ src/filesystem/FileInitializer.java
@@ -7,8 +7,26 @@ public class FileInitializer {
 
     private String username = System.getProperty("user.name");
     private String path = "C:\\Users\\"+username+"\\Documents\\SudokuSolver\\src";
+    private File mainFolder = new File("C:\\Users\\"+username+"\\Documents\\SudokuSolver");
+    private File srcFolder = new File("C:\\Users\\"+username+"\\Documents\\SudokuSolver\\src");
+    private File imgFolder = new File("C:\\Users\\"+username+"\\Documents\\SudokuSolver\\src\\images");
     private File alreadyShifted = new File(path+"\\shifted.txt");
 
+    public FileInitializer(){
+        
+        createFileSystem();
+    }
+    
+    private void createFileSystem(){
+        
+        if(!mainFolder.exists())
+            mainFolder.mkdir();
+        if(!srcFolder.exists())
+            srcFolder.mkdir();
+        if(!imgFolder.exists())
+            imgFolder.mkdir();
+    }
+    
     public void setShifted(){
         try {
             alreadyShifted.createNewFile();
